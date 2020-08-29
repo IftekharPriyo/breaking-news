745ab3c3fb1af83162cf18060ecba827755e2c0f
IftekharPriyo <priyo.mobin123@gmail.com>
2020-08-29 20:53:06 +0600

    react-bootstrap comp added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import News from './Components/News/News';
+import Header from './Components/Header/Header';
+import TopHeadline from './Components/TopHeadline/TopHeadline';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>React Bootstrap</h2>
+      <Header></Header>
+      <TopHeadline></TopHeadline>
     </div>
   );
 }
